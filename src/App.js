def486ce52ecf35c397e9b67b6046bad2cde2fe5
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-10-14 23:46:21 +0100

    Added layout with navigation tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
-import { DatePicker } from "antd";
+import { DatePicker, Layout, Menu } from "antd";
 import "antd/dist/antd.css";
 
 function App() {
+  const { Header, Content, Footer } = Layout;
+
   return (
     <div className="App">
+      <Layout>
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content
+          className="site-layout"
+          style={{ padding: "0 50px", marginTop: 64 }}
+        >
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 380 }}
+          >
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Footer</Footer>
+      </Layout>
       <DatePicker />
     </div>
   );
