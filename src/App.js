45600b033f3e01bd05ac3e695a6f6f1ceeeb2a8a
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-10-14 23:35:53 +0100

    Tested antd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-
-import "./App.css";
+import { DatePicker } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <DatePicker />
     </div>
   );
 }
